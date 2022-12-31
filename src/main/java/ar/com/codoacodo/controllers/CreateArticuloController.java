b64361ec1ee5706e2a1184587f704e5fb259d9a6
Gladys Romero <111000044+Gladys077@users.noreply.github.com>
2022-12-31 20:58:28 -0300

    Final

--- src/main/java/ar/com/codoacodo/controllers/CreateArticuloController.java
+++ src/main/java/ar/com/codoacodo/controllers/CreateArticuloController.java
@@ -1,6 +1,7 @@
 package ar.com.codoacodo.controllers;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import ar.com.codoacodo.controllers.validators.CreateProductoValidator;
+import ar.com.codoacodo.controllers.validators.IValidable;
 import ar.com.codoacodo.dao.IArticuloDAO;
 import ar.com.codoacodo.dao.impl.ArticuloDAOMysqlImpl;
 import ar.com.codoacodo.domain.Articulo;
@@ -15,28 +18,88 @@ import ar.com.codoacodo.domain.Articulo;
 @WebServlet("/CreateArticuloController")
 public class CreateArticuloController extends HttpServlet{
 
+	//en este caso es un doPost porque recibimos los datos
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		//crear un articulo
-		//recibimos los parametros enviados por un formularios
-		String titulo = req.getParameter("titulo");//un string que representa el valor enviado en el input
-		String autor = req.getParameter("autor");
-		String img = req.getParameter("imagen");
-		Float precio = Float.parseFloat(req.getParameter("precio"));//String > Float.parseFloat()
+		// super.doPost(req, resp);
 
-		//instanciomos nuestro objeto
-		var articulo = new Articulo(img, titulo, autor, precio);
+		// crear un articulo
+		CreateArticuloRequest createRequest = new CreateArticuloRequest(req);
 		
-		//hacemos uso del dao
+		IValidable validator = new CreateProductoValidator(createRequest);
+		List<String> errors = validator.validate();
+		
+		//verifico si hay errores
+		if(!errors.isEmpty()) {
+			//volver a nuevo.jsp con la lista de errores
+			//guarda la lista en el request
+			req.setAttribute("errors", errors);
+			//redirect a otra pagina u otro servlet(Controller/WebServlet)
+			getServletContext().getRequestDispatcher("/nuevo.jsp").forward(req, resp);
+			return;
+		}
+		
+		// instanciamos nuestro objeto
+		var articulo = new Articulo(createRequest.getImg(), createRequest.getTitulo(), createRequest.getAutor(), createRequest.getPrecio());
+		
+		//hacemos uso del DAO e instanciamos nuestra implementacion
 		IArticuloDAO dao = new ArticuloDAOMysqlImpl();
-
-		//control de errores se vera con detalle en el curso de spring
+		
+		//control de errores se vera en curso de spring
 		try {
 			dao.create(articulo);
 		} catch (Exception e) {
-			e.printStackTrace();			
+			e.printStackTrace();
 		}
 		
 		//redirect a otra pagina u otro servlet(Controller/WebServlet)
 		getServletContext().getRequestDispatcher("/FindAllArticulosController").forward(req, resp);
 	}
 }
+
+	/*
+	 * @Override protected void doGet(HttpServletRequest req, HttpServletResponse
+	 * resp) throws ServletException, IOException { //super.doGet(req, resp);
+	 * 
+	 * Scanner teclado = new Scanner(System.in) ;
+	 * 
+	 * System.out.println("ingrese nombre del tema"); String titulo =
+	 * teclado.nextLine(); System.out.println("ingrese nombre del autor"); String
+	 * autor = teclado.nextLine(); System.out.println("ingrese url de la imagen");
+	 * String img = teclado.nextLine(); System.out.println("ingrese el precio");
+	 * Float precio =teclado.nextFloat(); //instanciamos el articulo con dichos
+	 * parametros var articulo = new Articulo(img, titulo, autor, precio);
+	 * 
+	 * 
+	 * IArticuloDAO dao = new ArticuloDAOMysqlImpl(); System.out.println("antes" +
+	 * articulo);
+	 * 
+	 * try { dao.create(articulo); System.out.println("despues" + articulo); } catch
+	 * (Exception e) { e.printStackTrace(); } }
+	 */
+
+	// aca: aplicacion de consola-----------------------------------------
+	/*public static void main(String[] args) throws Exception {
+
+		// crear un articulo
+		// suponemos que recibimos los parametros enviados por un formularios
+		Scanner teclado = new Scanner(System.in);
+
+		System.out.println("ingrese nombre del tema");
+		String titulo = teclado.nextLine();
+		System.out.println("ingrese nombre del autor");
+		String autor = teclado.nextLine();
+		System.out.println("ingrese url de la imagen");
+		String img = teclado.nextLine();
+		System.out.println("ingrese el precio");
+		Float precio = teclado.nextFloat();
+		// instanciamos el articulo con dichos parametros
+		var articulo = new Articulo(img, titulo, autor, precio);
+
+		IArticuloDAO dao = new ArticuloDAOMysqlImpl();
+		System.out.println("antes" + articulo);
+
+		dao.create(articulo);
+		System.out.println("despues" + articulo);
+		System.out.println("�Desea continuar?");
+	}*/
+
