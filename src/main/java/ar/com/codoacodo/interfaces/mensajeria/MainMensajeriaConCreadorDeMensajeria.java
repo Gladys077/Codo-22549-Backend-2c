ff6d250743cff69bfdb56d55ac95d05d756adc4a
Gladys Romero <111000044+Gladys077@users.noreply.github.com>
2022-12-31 22:02:56 -0300

    Update MainMensajeriaConCreadorDeMensajeria.java

--- src/main/java/ar/com/codoacodo/interfaces/mensajeria/MainMensajeriaConCreadorDeMensajeria.java
+++ src/main/java/ar/com/codoacodo/interfaces/mensajeria/MainMensajeriaConCreadorDeMensajeria.java
@@ -3,18 +3,22 @@ package ar.com.codoacodo.interfaces.mensajeria;
 public class MainMensajeriaConCreadorDeMensajeria {
 
 	public static void main(String[] args) {
-		String target = "";
+		String target = "C";		
+		String mensaje = "aprendiendo interfaces en java";
 		
-		String mensaje = "Aprendiendo interfaces en java";
-				
-		IEnviar msj = CreadorDeMensajeria.crearMensajeria(target);
+		//al haber creado un metodo static en CreadorDeMensajeria, no necesito instanciar la clase
+		//CreadorDeMensajeria. Puedo hacer clase.metodo
 		
+		
+		//eso ya me retorna IEnviar algo(mensaje en este caso)
+		IEnviar msj = CreadorDeMensajeria.crearMensajeria(target);//con esto queda desacoplado la creacion
+																//ya no necesitamos la clase Mensajero
 		msj.enviar(mensaje);
-	}
+		
+		/*if(msj != null) {
+		msj.enviar(mensaje);*/ //este seria otro tipo de verificador
+							//para que no de null y tire error. nosotros optamos por la clase Default 
+	}	
+	
 }
-	//este newEnviar etc es lo mismo que instanciar la clase y 
-		//luego pasarlo por parámetro, en lugar de la linea 13
-		//tendría que poner:
-		//var msj = new EnviarMailImpl();
-		//mensajero.setMensajeria(msj);
 
