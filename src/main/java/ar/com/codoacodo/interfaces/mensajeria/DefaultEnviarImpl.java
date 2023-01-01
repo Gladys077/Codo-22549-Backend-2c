bbc0e5e98a4c37dcfcd12bbbe133f7a79e57ee21
Gladys Romero <111000044+Gladys077@users.noreply.github.com>
2022-12-31 21:55:36 -0300

    default

--- src/main/java/ar/com/codoacodo/interfaces/mensajeria/DefaultEnviarImpl.java
+++ src/main/java/ar/com/codoacodo/interfaces/mensajeria/DefaultEnviarImpl.java
@@ -1,9 +1,8 @@
 package ar.com.codoacodo.interfaces.mensajeria;
 
-public class DefaultEnviarImpl implements IEnviar {
+public class DefaultEnviarImpl implements IEnviar{
 	
 	public void enviar(String msj) {
-		System.out.println("Implementación por defecto");
+		System.out.println("Implementacion por defecto! para target:" + msj);
 	}
-
 }
