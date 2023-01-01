ebfb1e8ed48d565855b2809f4d07c369104b9a4a
Gladys Romero <111000044+Gladys077@users.noreply.github.com>
2022-12-31 21:57:24 -0300

    Update EnviarHDImpl.java

--- src/main/java/ar/com/codoacodo/interfaces/mensajeria/EnviarHDImpl.java
+++ src/main/java/ar/com/codoacodo/interfaces/mensajeria/EnviarHDImpl.java
@@ -1,16 +1,16 @@
 package ar.com.codoacodo.interfaces.mensajeria;
 
+//creamos una clase que va a implementar la interface IEnviar
 public class EnviarHDImpl implements IEnviar{
 	
-	private String emailTarget = "destino@mail.com";
-	private String emailSource = "yo@yo.com";
-	private String provider = "gmail";
+	//esta clase esta encapsulando sus atributos y solo ella accede a los mismos
+	private String fileSystemPath = "/temp/miusuario";
 	//etc
 	
-	//cumplo el contrato
+	//esta clase debe cumplir el contrato entonces debemos escribir el metodo de 
+	//como lo va a cumplir
 	public void enviar(String msj) {
-		System.out.println("Enviando mail a " + emailTarget);
-		System.out.println("Enviando desde " + emailSource);
-		System.out.println("Usando " + provider);
+		System.out.println("Grabando en: " + fileSystemPath);
+		System.out.println("Mensaje: " + msj);
 	}
 }
